4d70f28cfd06105b043f22ead029dc3a4538aea8
Risvi-Tareq <ivsirqerat@gmail.com>
2022-12-05 13:57:17 -0800

    setup

--- Assets/tests/Employee.test.js
+++ Assets/tests/Employee.test.js
@@ -1,4 +1,4 @@
-const Employee = require("../lib/Employee");
+ const Employee = require("../lib/Employee");
 
 describe("Testing Employee Class", () => {
     it("should be able to create instance", () => {
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 10 Object-Oriented Programming: Team Profile Generator
+cd # 10 Object-Oriented Programming: Team Profile Generator
 
 ## Your Task
 
